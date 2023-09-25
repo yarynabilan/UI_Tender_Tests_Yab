cbaa902c7b5b8362bc20c140d4e9e1a29dc88f34
Yaryna Bilan <yabi@rib-software.dk>
2023-09-25 15:46:16 +0300

    DataInputRequirementTest-File upload Req (first test)

---
+++ src/main/java/main/subReq/dataInput/FileUploadReq.java
@@ -0,0 +1,36 @@
+package main.subReq.dataInput;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class FileUploadReq {
+    private WebDriver driver;
+
+    public FileUploadReq(WebDriver driver) {
+        this.driver = driver;
+    }
+    private By fileUploadInput = By.xpath("//input[@class=\"contentNormal\"]");
+    private By descriptionFiled = By.xpath("//textarea[@class=\"contentNormal\" and @name=\"description\"]");
+    private By okButton = By.xpath("//input[@name='x' and @value='OK']");
+
+    public WebElement fileUploadInput() {return driver.findElement(fileUploadInput);}
+    public WebElement descriptionFiled() {return driver.findElement(descriptionFiled);}
+    public WebElement okButton() { return driver.findElement(okButton);}
+
+    public FileUploadReq fillInNameOfFileUploadReq(String fileUploadName) {
+        WebElement fileUploadInput = fileUploadInput();
+        fileUploadInput.clear();
+        fileUploadInput.sendKeys(fileUploadName);
+        return this;
+    }
+    public FileUploadReq fillInDescriptionField(String newDescription) {
+        WebElement descriptionFiled = descriptionFiled();
+        descriptionFiled.clear();
+        descriptionFiled.sendKeys(newDescription);
+        return this;
+    }
+    public void clickOkButton() {
+        okButton().click();
+    }
+}
