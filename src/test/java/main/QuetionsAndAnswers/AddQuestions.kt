a17f123098861cbf3865e9dd5422492bc0db6687
yab <yarina.bilan@rib-software.com>
2024-03-29 17:23:23 +0200

    refactoring - AddQuestions Tests

--- src/test/java/main/QuetionsAndAnswers/AddQuestions.kt
+++ src/test/java/main/QuetionsAndAnswers/AddQuestions.kt
@@ -5,83 +5,39 @@ import main.PublicTenderPage
 import main.TendersPage
 import main.questionAndAnswers.NewQuestions
 import org.testng.annotations.Test
+import org.testng.annotations.BeforeMethod
 
-class AddQuestions @Test constructor() : BaseTest() {
-    private val addQuestions: AddQuestions? = null
 
-    @Test
-    @Throws(InterruptedException::class)
-    fun CreateNewQuestionToPublish() {
-        val tendersPage = TendersPage(driver)
+class AddQuestions : BaseTest() {
+    private lateinit var tendersPage: TendersPage
+    private lateinit var publicTenderPage: PublicTenderPage
+    private lateinit var newQuestions: NewQuestions
+
+    @BeforeMethod
+    fun setUpTest() {
+        tendersPage = TendersPage(driver)
+        publicTenderPage = PublicTenderPage(driver)
+        newQuestions = NewQuestions(driver)
         tendersPage.switchToBrowserFrame()
-        val publicTenderPage = PublicTenderPage(driver)
         publicTenderPage.clickOnElement(publicTenderPage.questionsAndAnswersTab)
-        driver!!.switchTo().defaultContent()
-        val newQuestions = NewQuestions(driver!!)
+        driver.switchTo().defaultContent()
         tendersPage.switchToNavigationFrame()
         newQuestions.clickOnAddNewQuestionButton()
         tendersPage.switchToNewWindowTest()
+    }
+    @Test
+    fun createNewQuestionToPublish() {
         newQuestions.fillInQuestionText("It will be published - Admin 1.")
-        newQuestions.confirmQuestionOkSend()
-        Thread.sleep(3000)
-    }
+        newQuestions.confirmQuestionOkSend() }
 
     @Test
-    @Throws(InterruptedException::class)
-    fun CreateNewQuestionToDraft() {
-        val tendersPage = TendersPage(driver)
-        tendersPage.switchToBrowserFrame()
-        val publicTenderPage = PublicTenderPage(driver)
-        publicTenderPage.clickOnElement(publicTenderPage.questionsAndAnswersTab)
-        driver!!.switchTo().defaultContent()
-        val newQuestions = NewQuestions(driver!!)
-        tendersPage.switchToNavigationFrame()
-        newQuestions.clickOnAddNewQuestionButton()
-        tendersPage.switchToNewWindowTest()
+    fun createNewQuestionToDraft() {
         newQuestions.fillInQuestionText("It will be draft 2.Admin")
-        newQuestions.confirmQuestionOkSend()
+        newQuestions.confirmQuestionOkSend() }
+
+    @Test
+    fun createNewQuestionToUnanswer() {
+        newQuestions.fillInQuestionText("It will be unAnswered.Admin")
+        newQuestions.confirmQuestionOkSend() }
     }
-
-    init {
-        val tendersPage = TendersPage(driver)
-        tendersPage.switchToBrowserFrame()
-        val publicTenderPage = PublicTenderPage(driver)
-        publicTenderPage.clickOnElement(publicTenderPage.questionsAndAnswersTab)
-        driver!!.switchTo().defaultContent()
-        val newQuestions = NewQuestions(driver!!)
-        val mainWindowHandle = driver!!.windowHandle
-        tendersPage.switchToNavigationFrame()
-        newQuestions.clickOnAddNewQuestionButton()
-        tendersPage.switchToNewWindowTest()
-        newQuestions.fillInQuestionText("It will be unanswered - 1 Admin")
-        Thread.sleep(5000)
-        newQuestions.confirmQuestionOkSend()
-        driver!!.switchTo().window(mainWindowHandle)
-        tendersPage.switchToNavigationFrame()
-        Thread.sleep(3000)
-        newQuestions.clickOnAddNewQuestionButton()
-        Thread.sleep(3000)
-        tendersPage.switchToNewWindowTest()
-        newQuestions.fillInQuestionText("It will be published - 2 Admin")
-        newQuestions.confirmQuestionOkSend()
-
-        driver!!.switchTo().window(mainWindowHandle)
-        tendersPage.switchToNavigationFrame()
-        Thread.sleep(3000)
-        newQuestions.clickOnAddNewQuestionButton()
-        Thread.sleep(3000)
-        tendersPage.switchToNewWindowTest()
-        newQuestions.fillInQuestionText("It will be draft 3 Admin")
-        newQuestions.confirmQuestionOkSend()
-
-        driver!!.switchTo().window(mainWindowHandle)
-        tendersPage.switchToNavigationFrame()
-        Thread.sleep(3000)
-        newQuestions.clickOnAddNewQuestionButton()
-        Thread.sleep(3000)
-        tendersPage.switchToNewWindowTest()
-        newQuestions.fillInQuestionText("test")
-        newQuestions.confirmQuestionOkSend()
-    }
-}
-
+//TODO: 1. add attachment to the question
