a821bc47f2df16f24235768da253a81cddfd1dd3
yab <yarina.bilan@rib-software.com>
2024-04-17 21:46:12 +0300

    users' types setUp

--- src/test/java/main/BaseTest.kt
+++ src/test/java/main/BaseTest.kt
@@ -1,5 +1,6 @@
 package main
 
+import org.testng.annotations.Parameters
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import org.testng.annotations.AfterMethod
@@ -8,23 +9,31 @@ import java.util.concurrent.TimeUnit
 
 open class BaseTest {
     protected open lateinit var driver: WebDriver
-
+    private lateinit var username: String
+    private lateinit var password: String
     @BeforeMethod
-    @Throws(InterruptedException::class)
-    fun setUp() {
+    @Parameters("username", "password")
+    fun setUp(username: String, password: String) {
+        this.username = username
+        this.password = password
+
+
         driver = ChromeDriver()
         driver.manage().window().maximize()
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS)
         driver.get("https://testdociaweb.byggeweb.dk/controller?cmd=company7.login.form&language=English&extern=1")
         val loginPage = LoginPage(driver)
-        loginPage.loginSuccessful("ribtestuser@gmail.com", "TestPassword#1")
+        loginPage.loginSuccessful(this.username, this.password)
         Thread.sleep(3000)
 
         val mainPage = MainPage(driver)
         mainPage.navigateToApplications().applications().click()
+        Thread.sleep(3000)
         val applicationsPage = ApplicationsPage(driver)
         Thread.sleep(3000)
         applicationsPage.tenderLink().click()
+        Thread.sleep(3000)
+
         val tendersPage = TendersPage(driver)
         tendersPage.openPublicTender()
         val mainWindowHandle = driver.getWindowHandle()
